f329abbc226133b913b93a29fa96f456379dd40e
23P31A4411 <147791804+23P3A14411@users.noreply.github.com>
2024-01-02 23:18:57 +0530

    #include<stdio.h> int main() {     int i,j,k,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=i;j<n;j++)         {             printf(" ");         }         for(k=1;k<=i;k++)         {             printf("%c ",64+n-i+1);         }         printf(" ");     } }

---
+++ Pattern_6.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=i;k++)
+        {
+            printf("%c ",64+n-i+1);
+        }
+        printf("
+");
+    }
+}
